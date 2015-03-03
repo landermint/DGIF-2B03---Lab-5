0a3baaec097deec47cc2048b70b35ae577d45765
landermint <jacklambermont@jacks-mbp.acs.ocad.ca>
2015-03-03 17:16:52 -0500

    removed comments

--- 2.2/sketch2.2/sketch2.2.ino
+++ 2.2/sketch2.2/sketch2.2.ino
@@ -1,11 +1,11 @@
 
-int photocellPin = 0; // the cell and 10K pulldown are connected to a0
-int photocellReading; // the analog reading from the sensor divider
+int sensePin = 0;
+int senseRead;
 void setup(void) {
   Serial.begin(9600);
 }
 void loop(void) {
-  photocellReading = analogRead(photocellPin);
-  Serial.println(photocellReading); // the raw analog reading
+  senseRead = analogRead(sensePin);
+  Serial.println(senseRead);
 }
 
